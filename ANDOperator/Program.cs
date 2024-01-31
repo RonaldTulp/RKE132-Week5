07043b602b23896f6d94ef0a690948f8c6d8dd15
Ronald <ronald.tulp@tktk.ee>
2024-01-31 22:38:05 +0200

    week5

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,17 @@
+﻿//Math >= 90; Biology >= 90; Chemistry >=90;
+
+int Math, Bio, Chem;
+
+Console.WriteLine("Sisestage oma Matemaatika tulemus:");
+Math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Sisestage oma Bioloogia tulemus:");
+Bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Sisestage oma Keemia tulemus:");
+Chem = Int32.Parse(Console.ReadLine());
+
+if (Math >= 90 && Bio >= 90 && Chem >= 90)
+    { Console.WriteLine("Palju õnne, Te olete vastuvõetud!"); }
+else
+    { Console.WriteLine("Teie taoltust ei saa heaks kiita."); }
---
+++ OROperator/Program.cs
@@ -0,0 +1,18 @@
+﻿//Math >= 90 OR Chemistry >= 90
+//Math && Chem || Math && Bio || Chem && Bio
+
+int Math, Bio, Chem;
+
+Console.WriteLine("Sisestage oma Matemaatika tulemus:");
+Math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Sisestage oma Bioloogia tulemus:");
+Bio = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Sisestage oma Keemia tulemus:");
+Chem = Int32.Parse(Console.ReadLine());
+
+if ((Math >= 90 && Chem >= 90) || (Math >= 90 && Bio >= 90) || (Bio >= 90 && Chem >= 90))
+{ Console.WriteLine("Palju õnne, Te olete vastuvõetud!"); }
+else
+{ Console.WriteLine("Teie taoltust ei saa heaks kiita."); }
